b8fc7cf35cedf7866d224aff9a7e69378b5af561
apprenti2012 <munoz.yannis04@gmail.com>
2025-12-02 18:21:37 +0100

    Refactor article filtering logic in JavaScript

--- htdocs/javascript.js
+++ htdocs/javascript.js
@@ -40,10 +40,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.getElementById("refresh").addEventListener("click", function() {
     const input = document.getElementById("searchBar").value.toLowerCase();
-    const articles = document.querySelectorAll("#articles a"); // tous les liens d’articles
-    articles.forEach(article => {
+    const articles = document.querySelectorAll("#articles a"); // tous les liens d’articles  
+  articles.forEach(article => {
         const text = article.textContent.toLowerCase();
-        if (text.includes(input)) {
+        if (input === "" ||text.includes(input)) {
             article.style.display = "inline-block";
         } else {
             article.style.display = "none";
